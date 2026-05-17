714ddf54090f091c4be08ea5a6555d394cbb2ad3
Michael Danu Ekklasiya <michaeldanuekklasiya@gmail.com>
2026-05-18 00:21:25 +0700

    Fix Vercel 500 error by setting storage paths to /tmp

--- api/index.php
+++ api/index.php
@@ -1,4 +1,20 @@
 <?php
 
+// Set directories to /tmp for Vercel's read-only filesystem
+$_SERVER['APP_CONFIG_CACHE'] = '/tmp/config.php';
+$_SERVER['APP_EVENTS_CACHE'] = '/tmp/events.php';
+$_SERVER['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
+$_SERVER['APP_ROUTES_CACHE'] = '/tmp/routes.php';
+$_SERVER['APP_SERVICES_CACHE'] = '/tmp/services.php';
+
+$compiledViewPath = '/tmp/storage/framework/views';
+if (!is_dir($compiledViewPath)) {
+    mkdir($compiledViewPath, 0777, true);
+}
+$_SERVER['VIEW_COMPILED_PATH'] = $compiledViewPath;
+
+$_ENV['LOG_CHANNEL'] = 'stderr';
+$_ENV['SESSION_DRIVER'] = 'cookie';
+
 // Forward Vercel serverless requests to normal Laravel public/index.php
 require __DIR__ . '/../public/index.php';
